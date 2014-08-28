ab51708dbd16a915d10a531b5bd704aed9955671
Yurun Sun <yurunsun@gmail.com>
2014-08-28 16:38:57 +0800

    change director structure

---
+++ src/machine.cpp
@@ -0,0 +1,40 @@
+#include "stdafx.h"
+#include "machine.h"
+
+
+#define CMD_FORMAT "/usr/bin/sshpass -p 15j4h8y-ob ssh -p 4353 root@%s"
+
+Machine *machine_new(const std::string &login, const std::string &name, int vtrows, int vtcols)
+{
+    Machine *m = new Machine;
+    m->login = login;
+    m->name = name;
+    m->tag = false;
+    m->alive = true;
+    m->vt = rote_vt_create(vtrows, vtcols);
+    m->pid = rote_vt_forkpty(m->vt, m->login.c_str());  /* build the command line and fork an ssh to the given machine */
+    return m;
+}
+
+void machine_destroy(Machine *m) {
+    if (!m) return;
+    rote_vt_destroy(m->vt);
+    delete m;
+    m = nullptr;
+}
+
+void machine_rename(Machine *m, char *newname) {
+    if (!m) return;
+    m->name = std::string(newname);
+}
+
+void machine_tag_push(Machine *m) {
+    if (m->tagstack_count >= TAGSTACK_SIZE) return;
+    m->tagstack[m->tagstack_count++] = m->tag;
+}
+
+void machine_tag_pop(Machine *m) {
+    if (!m->tagstack_count) return;
+    m->tag = m->tagstack[--m->tagstack_count];
+}
+
